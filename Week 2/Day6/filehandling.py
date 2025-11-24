fbaad1be0c9a530936a33bb95588cb88e5289348
ritik-avisoft <ritik.ranjan@avisoft.io>
2025-11-24 18:35:43 +0530

    from win day 06

---
+++ Week 2/Day6/filehandling.py
@@ -0,0 +1,24 @@
+# #creating a file using a,w,mode 
+
+# file=open("newfile.txt",'r')
+# content=file.read()
+# print(content)
+# file.close
+
+
+
+# #writig file 
+# f= open("file.txt",'w+')
+# f.write("Hello is Wrritten by 'w' mode.")
+# f.seek(0)
+# print(f.read())
+# f.close()
+
+# f = open("file.txt", "r")
+# # content = f.read()
+# print(f.read())
+# # print(content)
+# f.close()
+
+# import os
+# os.remove("newfile.txt")
---
+++ Week 2/Day6/newCsv.py
@@ -0,0 +1,21 @@
+#reading a CSV 
+import csv
+
+# with open("student.csv",'r') as file:
+#     content=csv.reader(file)
+#     for i in content:
+#         print(i)
+
+# with open("student.csv",'r') as file:
+#     content=csv.DictReader(file)
+#     for i in content:
+#         print(i)
+
+
+# import csv
+
+# with open("employees.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Name", "Salary", "City"])
+#     writer.writerow(["Ritik", 25000, "Delhi"])
+#     writer.writerow(["Aman", 40000, "Noida"])
